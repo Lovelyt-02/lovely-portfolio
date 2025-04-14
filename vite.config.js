e385add0de4f5286591a9c1eb84cbb5c3eaf9172
Lovely Tiwari <lovelyt7989@gmail.com>
2025-04-14 17:53:31 +0530

    improve design

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lovely-portfolio/',
+  base: '/',
   plugins: [react({ fastRefresh: true })], // Ensures React Fast Refresh is enabled
   css: {
     preprocessorOptions: {
